4168bca180c99dbc55086604a459fbb14ae59d3e
Eliot Houghton <eliot@mackmangroup.co.uk>
2014-09-10 13:11:49 +0100

    Fixed numerous styling issues Added subcategories into the breadcrumb function List categories/subcategories on homepage

--- wp-content/themes/knowledge/functions.php
+++ wp-content/themes/knowledge/functions.php
@@ -122,44 +122,46 @@ if ( ! function_exists( 'reverie_entry_meta' ) ) {
 ?>
 
 <?php
-function the_breadcrumb() {
+function the_breadcrumb() 
+{
     global $post;
     echo '<ul id="breadcrumbs">';
-    if (!is_home()) {
+    if ( !is_home() ) 
+	{
         echo '<li><a href="';
         echo get_option('home');
         echo '">';
         echo 'Home';
         echo '</a></li><li class="separator"> / </li>';
-        if (is_category() || is_single()) {
-            echo '<li>';
+        if ( is_category() || is_single() ) 
+		{
+			$categories = get_the_category();
+			$this_cat_ID = $categories[0]->cat_ID;
+			$this_cat_name = $categories[0]->cat_name;
+			$this_cat_url = get_category_link($this_cat_ID);
+
+			$parent_name = get_cat_name($categories[0]->category_parent);
+			$parent_link = get_category_link($categories[0]->category_parent);
+			
+			if ( $categories[0]->category_parent !== 0 ) 
+			{
+				echo '<li class="test"><a href="';
+				echo $parent_link;
+				echo '">';
+				echo $parent_name;
+				echo '</a><li class="separator"> / </li><li> ';
+			}
+		
+            echo '<li class="cat">';
             the_category(' </li><li class="separator"> / </li><li> ');
-            if (is_single()) {
-                echo '</li><li class="separator"> / </li><li>';
+            if ( is_single() ) 
+			{
+                echo '</li><li class="separator"> / </li><li class="single">';
                 the_title();
                 echo '</li>';
             }
-        } elseif (is_page()) {
-            if($post->post_parent){
-                $anc = get_post_ancestors( $post->ID );
-                $title = get_the_title();
-                foreach ( $anc as $ancestor ) {
-                    $output = '<li><a href="'.get_permalink($ancestor).'" title="'.get_the_title($ancestor).'">'.get_the_title($ancestor).'</a></li> <li class="separator">/</li>';
-                }
-                echo $output;
-                echo '<strong title="'.$title.'"> '.$title.'</strong>';
-            } else {
-                echo '<li><strong> '.get_the_title().'</strong></li>';
-            }
-        }
+        } 
     }
-    elseif (is_tag()) {single_tag_title();}
-    elseif (is_day()) {echo"<li>Archive for "; the_time('F jS, Y'); echo'</li>';}
-    elseif (is_month()) {echo"<li>Archive for "; the_time('F, Y'); echo'</li>';}
-    elseif (is_year()) {echo"<li>Archive for "; the_time('Y'); echo'</li>';}
-    elseif (is_author()) {echo"<li>Author Archive"; echo'</li>';}
-    elseif (isset($_GET['paged']) && !empty($_GET['paged'])) {echo "<li>Blog Archives"; echo'</li>';}
-    elseif (is_search()) {echo"<li>Search Results"; echo'</li>';}
     echo '</ul>';
 }
 ?>
--- wp-content/themes/knowledge/page-home.php
+++ wp-content/themes/knowledge/page-home.php
@@ -13,8 +13,6 @@ get_header(); ?>
 	<!-- Row for main content area -->
 	<div class="small-12 large-12 columns" id="content" role="main">
 	
-	<?php echo wp_list_categories('orderby=name&title_li='); ?> 
-	
 	<?php /* Start loop */ ?>
 	<?php while (have_posts()) : the_post(); ?>
 		<article <?php post_class() ?> id="post-<?php the_ID(); ?>">
@@ -30,6 +28,84 @@ get_header(); ?>
 			</footer>
 		</article>
 	<?php endwhile; // End the loop ?>
+	
+	<?php //get all categories then display all posts in each term
+		$taxonomy = 'category';
+		$param_type = 'category__in';
+		$term_args=array( 'orderby' => 'name', 'order' => 'ASC' );
+		$terms = get_terms($taxonomy, $term_args);
+		
+		if ($terms) 
+		{
+			foreach( $terms as $term ) 
+			{
+				$args=array(
+					"$param_type" => array($term->term_id),
+					'post_type' => 'post',
+					'post_status' => 'publish',
+					'posts_per_page' => -1,
+					'caller_get_posts'=> 1
+				);
+				
+				$my_query = null;
+				$my_query = new WP_Query($args);
+				
+				if( $my_query->have_posts() ) 
+				{
+					$querychildren = get_category_children($term->term_id);
+					if( $querychildren != ""  )
+					{
+						echo '<div class="category section">';
+							echo '<h3>';
+								echo $term->name;
+							echo '</h3>';
+							$childcategories =  get_categories('child_of='.$term->term_id);  
+							foreach  ($childcategories as $category) 
+							{
+								//Display the sub category information using $category values like $category->cat_name
+								echo '<div class="subcategory">';
+								echo '<h5>'.$category->name.'</h5>';
+								echo '<ul>';
+
+								foreach (get_posts('cat='.$category->term_id) as $post) 
+								{
+									setup_postdata( $post );
+									echo '<li><a href="'.get_permalink($post->ID).'">'.get_the_title().'</a></li>';   
+								}  
+								echo '</ul>';
+								echo '</div>';
+							}
+							echo '<ul>';?>
+								<?php while ($my_query->have_posts()) : $my_query->the_post(); ?>
+									<li><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></li>
+								<?php endwhile;?><?php
+							echo '</ul>';
+						echo '</div>';
+					?>
+					<?php
+					}
+					else
+					{
+						if( $term->parent == 0 )
+						{?>
+						<div class="category section">
+							<h3>
+								<?php echo $term->name;?>
+							</h3>
+							<ul>
+								<?php while ($my_query->have_posts()) : $my_query->the_post(); ?>
+									<li><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></li>
+								<?php endwhile;?>
+							</ul>
+						</div>
+						<?php
+						}
+					}
+				}
+			}
+		}
+		wp_reset_query();  // Restore global post data stomped by the_post().
+	?>
 
 	</div>
 		
