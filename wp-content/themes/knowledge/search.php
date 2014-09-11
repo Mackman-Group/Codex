93c96f9e89fe43bf90e677dc96f5d10dda71a8c8
Eliot Houghton <eliot@mackmangroup.co.uk>
2014-09-11 14:33:25 +0100

    Searching for the private tag wont work now

--- wp-content/themes/knowledge/search.php
+++ wp-content/themes/knowledge/search.php
@@ -10,9 +10,25 @@
 	<?php if ( have_posts() ) : ?>
 	
 		<?php /* Start the Loop */ ?>
-		<?php while ( have_posts() ) : the_post(); ?>
-			<?php get_template_part( 'content', get_post_format() ); ?>
-		<?php endwhile; ?>
+		<?php 
+		while ( have_posts() ) : the_post();
+			if ( ! is_user_logged_in() )
+			{
+				if( ! has_tag("private") )
+				{
+					get_template_part( 'content', get_post_format() );  
+				}
+				else
+				{
+					get_template_part( 'content', 'none' );
+				}
+			}
+			else
+			{
+				get_template_part( 'content', get_post_format() );
+			}
+		endwhile; 
+		?>
 		
 		<?php else : ?>
 			<?php get_template_part( 'content', 'none' ); ?>
