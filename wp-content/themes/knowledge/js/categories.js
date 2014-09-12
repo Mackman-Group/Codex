a4a62cdbb9ae5496c1b25510c116eedfbf7b6165
Eliot Houghton <eliot@mackmangroup.co.uk>
2014-09-12 10:54:55 +0100

    Fixed up categories.js

--- wp-content/themes/knowledge/js/categories.js
+++ wp-content/themes/knowledge/js/categories.js
@@ -1,13 +1,26 @@
 jQuery(document).ready(function() 
 {
-	// Strip out any empty categories
-	jQuery('.category.section, .subcategory').each(function(i, obj) 
+	// Strip out any empty sub-categories
+	jQuery('.subcategory').each(function(i, obj) 
 	{
+		//console.log(jQuery(obj));
+		if ( jQuery(obj).children('ul').children().length == 0 )
+		{
+			jQuery(obj).remove();
+		}
+	});
+	
+	// Strip out any empty categories
+	jQuery('.category.section').each(function(i, obj) 
+	{
+		console.log(jQuery(obj));
 		if ( jQuery(obj).children('.subcategory').length == 0 )
 		{
+			console.log(jQuery(obj).children('.subcategory').length);
 			if ( jQuery(obj).children('ul').children().length == 0 )
 			{
-				jQuery(obj).css('display', 'none');
+				console.log(jQuery(obj).children('ul').children().length);
+				jQuery(obj).remove();
 			}
 		}
 	});
